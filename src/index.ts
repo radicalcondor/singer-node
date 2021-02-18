c0817a09423343d73922b23290b560a88f5e2d43
Hassan Khan <hassan@radicalcondor.dev>
2021-02-18 01:19:09 +0000

    chore: remove dead code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,3 @@ export * from './spec/Config';
 export * from './spec/messages/Record';
 export * from './spec/messages/Schema';
 export * from './spec/messages/State';
-
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
