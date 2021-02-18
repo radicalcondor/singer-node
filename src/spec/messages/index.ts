f93ecd6a5b24daef79f3486468fb6d53a798f752
Cai Leao <caina@qudini.com>
2021-02-18 20:53:46 +0000

    perf: change switch case to object lookup

--- src/spec/messages/index.ts
+++ src/spec/messages/index.ts
@@ -1,19 +1,23 @@
+import { JsonSchemaType } from '../../types';
 import { MessageType, MessageTypes } from './Message';
-import { Record, RecordType } from './Record';
+import { Record as RecordMessage, RecordType } from './Record';
 
 const isRecord = (message: MessageType): message is RecordType => {
   return (message as RecordType).type === MessageTypes.RECORD;
 };
 
+const foo: Partial<Record<MessageTypes, (m: any) => false | RecordMessage<JsonSchemaType>>> = {
+  [MessageTypes.RECORD]: (message: any) => isRecord(message) && new RecordMessage(message),
+}
+
 export const parseMessage = (messageString: string) => {
   const message = JSON.parse(messageString);
 
-  switch (message.type) {
-    case MessageTypes.RECORD:
-      if (isRecord(message)) {
-        return new Record(message);
-      }
+  const parser = foo[message.type as MessageTypes];
+
+  if (!parser) {
+    throw new Error('Invalid message type');
   }
 
-  throw new Error('Invalid message type');
+  return parser(message) as RecordMessage<JsonSchemaType>;
 };
