5298781bd8b9ccbe9699279bfebff290485c0ef4
Hassan Khan <hassan@radicalcondor.dev>
2021-02-18 01:20:49 +0000

    feat: implement errors as per `singer-python`

---
+++ src/spec/errors/index.ts
@@ -0,0 +1,42 @@
+import { BaseError } from "make-error";
+
+/**
+ * The base `Error` class for Singer.
+ */
+export class SingerError extends BaseError {
+
+  /**
+   * Create an error with a multi-line error message. The first line is the
+   * error's class name. The subsequent lines are the message that class was
+   * created with.
+   *
+   * @param message The error message. Optional
+   */
+  constructor(message?: string) {
+    super(message);
+    this.message = [
+      this.constructor.name,
+      message,
+    ].join('\n');
+  }
+}
+
+/**
+ * The base class of errors encountered before discovery and before sync mode
+ */
+export class SingerConfigurationError extends SingerError {}
+
+/**
+ * The base class of errors encountered in discovery mode
+ */
+export class SingerDiscoveryError extends SingerError {}
+
+/**
+ * The base class of errors encountered in sync mode
+ */
+export class SingerSyncError extends SingerError {}
+
+/**
+ * This error is meant to be thrown when a Tap encounters a retryable request
+ */
+export class SingerRetryableRequestError extends SingerError {}
