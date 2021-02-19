4b7fee557caa2d53df61afbdb9b8d5f6fb55131d
Hassan Khan <hassan@radicalcondor.dev>
2021-02-19 01:35:34 +0000

    test: improve test coverage for `parseMessage()`

---
+++ src/spec/messages/parseMessage.spec.ts
@@ -0,0 +1,19 @@
+import { parseMessage } from './parseMessage';
+import {SingerError} from "../errors";
+
+describe(parseMessage.name, () => {
+  it('should fail to parse invalid JSON', () => {
+    const RAW_MESSAGE = '{"foo":"bar"';
+    expect(() => parseMessage(RAW_MESSAGE)).toThrow(SingerError);
+  });
+
+  it('should fail to parse an invalid message', () => {
+    const RAW_MESSAGE = '{"foo":"bar"}';
+    expect(() => parseMessage(RAW_MESSAGE)).toThrow(SingerError);
+  });
+
+  it('should fail to parse a message with an invalid type', () => {
+    const RAW_MESSAGE = '{"type":"FOO"}';
+    expect(() => parseMessage(RAW_MESSAGE)).toThrow(SingerError);
+  });
+});
