b6283d375fe464a38882f27e4197eb8d5e2bceb9
Hassan Khan <hassan@radicalcondor.dev>
2021-02-21 00:27:53 +0000

    fix: correctly export all modules

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import pkg from '../package.json';
+
+import * as singer from './';
+
+describe(pkg.name, () => {
+  it('should import successfully', () => {
+    expect(singer).toBeDefined();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import * as _bookmarks from './spec/bookmarks';
+import * as _errors from './spec/errors';
+import * as _logger from './spec/logger';
+
 export * from './spec/Config';
 export * from './spec/messages/RecordMessage';
 export * from './spec/messages/SchemaMessage';
 export * from './spec/messages/StateMessage';
+
+export const bookmarks = _bookmarks;
+export * from './spec/Catalog';
+export * from './spec/CatalogEntry';
+export * from './spec/Config';
+export const errors = _errors;
+export const logger = _logger;
+export * from './spec/Metric';
